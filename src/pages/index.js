56b16c59e65c824bdf913749d8a131f6815e0a00
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-09-09 20:40:18 +0300

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation"
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
+import Timeline from "../components/Timeline/Timeline";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
 
@@ -15,6 +16,7 @@ const Home = () => {
             </Section>
             <Projects />
             <Technologies />
+            {/* <Timeline /> */}
         </Layout>
     );
 };
